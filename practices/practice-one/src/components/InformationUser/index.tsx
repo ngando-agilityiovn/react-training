938601659252f71bbfa70d2633d732a3401ce4c8
ngan.do <ngan.do@asnet.com.vn>
2023-10-05 23:21:16 +0700

    feat: create user information component

---
+++ practices/practice-one/src/components/InformationUser/index.tsx
@@ -0,0 +1,23 @@
+// styles
+import styles from './index.module.css';
+
+interface InformationUserProps {
+  image: string;
+  title: string;
+  alt: string;
+  description?: string;
+}
+
+const UserInformation: React.FC<InformationUserProps> = ({ image, title, alt, description }) => {
+  return (
+    <div className={`${styles.wrap}`}>
+      <img className={`${styles.img}`} src={image} alt={alt} />
+      <div>
+        <div className={`${styles.title}`}>{title}</div>
+        <div className={`${styles.description}`}>{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UserInformation;
--- practices/practice-one/src/components/index.ts
+++ practices/practice-one/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as TextField } from './TextField';
 export { default as Button } from './Button';
 export { default as Tag } from './Tag';
+export { default as UserInformation } from './InformationUser';
