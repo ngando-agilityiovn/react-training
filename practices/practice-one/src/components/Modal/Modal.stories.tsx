c0cce3e9aa8d10a5abe647bda142bafa007415f4
Ngan.Do <ngan.do@asnet.com.vn>
2023-10-13 16:14:00 +0700

    fix: update modal

--- practices/practice-one/src/components/Modal/Modal.stories.tsx
+++ practices/practice-one/src/components/Modal/Modal.stories.tsx
@@ -1,18 +1,44 @@
-import { StoryObj, Meta } from '@storybook/react';
+import { Meta } from '@storybook/react';
+import { useCallback, useState } from 'react';
+
+// Components
 import { Modal } from '.';
+import { Button } from '..';
+
+// Types
+import { BUTTON_VARIANT } from '@/types';
+
 export default {
   title: 'Components/Modal',
   component: Modal
 } as Meta;
 
-type Story = StoryObj<typeof Modal>;
+const ModalWithHooks = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export const Default: Story = {
-  args: {
-    title: 'Do you want to delete this user ?',
-    children: 'text',
-    onClose: () => {
-      alert('Button component');
-    }
-  }
+  const handleClick = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  return (
+    <div>
+      <Button variant={BUTTON_VARIANT.PRIMARY} onClick={handleClick}>
+        Open Modal
+      </Button>
+
+      {isOpen && (
+        <Modal onClose={handleClose} title={'Do you want to delete this user?'}>
+          <div className=''>Content Modal</div>
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export const Default = {
+  render: () => ModalWithHooks()
 };
--- practices/practice-one/src/components/Modal/index.tsx
+++ practices/practice-one/src/components/Modal/index.tsx
@@ -1,13 +1,15 @@
-import { ReactNode } from 'react';
+import { ReactNode, memo, useRef } from 'react';
 
 // Types
 import { BUTTON_VARIANT } from '@/types';
 
 // Styles
 import styles from './index.module.css';
+import { useOutsideClick } from '@/hooks/useOutsideClick';
+import { Close } from '../Icons';
+import { Button } from '..';
 
 // Components
-import { Button } from '..';
 
 interface IProps {
   title: string;
@@ -15,14 +17,17 @@ interface IProps {
   onClose: () => void;
 }
 
-export const Modal: React.FC<IProps> = ({ children, title, onClose }) => {
+export const Modal: React.FC<IProps> = memo(({ children, title, onClose }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  useOutsideClick(ref, onClose);
+
   return (
     <div>
-      <div className={styles.modal}>
+      <div ref={ref} className={styles.modal}>
         <div className={styles.modalHeader}>
           <h2 className={styles.title}>{title}</h2>
-          <Button variant={BUTTON_VARIANT.ICON} onClick={onClose}>
-            x
+          <Button className={styles.btnClose} variant={BUTTON_VARIANT.ICON} onClick={onClose}>
+            <Close />
           </Button>
         </div>
         {children}
@@ -30,4 +35,4 @@ export const Modal: React.FC<IProps> = ({ children, title, onClose }) => {
       <div className={styles.overlay}></div>
     </div>
   );
-};
+});
