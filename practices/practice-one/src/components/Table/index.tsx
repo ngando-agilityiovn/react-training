a90df1ec9410decacccac1097a2fe0360b700cbf
ngan.do <ngan.do@asnet.com.vn>
2023-10-14 13:45:39 +0700

    fix: update home page

--- practices/practice-one/src/components/Table/index.tsx
+++ practices/practice-one/src/components/Table/index.tsx
@@ -1,20 +1,39 @@
 // Styles
-import { TAG_COLORS } from '@/types';
-import { Delete, Edit, Tag } from '..';
 import styles from './index.module.css';
+
+// Types
+import { TAG_COLORS } from '@/types';
+
+// Components
+import { Delete, Edit, Tag } from '..';
 
 export const Table: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <div className={styles.divTable}>
+    <div className={styles.main}>
+      <h2 className={styles.heading}>User</h2>
+      <div className={styles.table}>
         <div className={`${styles.headerRow} ${styles.active}`}>
-          <div className={styles.divCell}>
-            <div className={styles.area}>
-              <b className={styles.title}>User</b>
-            </div>
+          <div className={styles.cell}>
+            <b className={styles.title}>User</b>
+          </div>
+          <div className={styles.cell}>
+            <b className={styles.title}>Email address</b>
+          </div>
+          <div className={styles.cell}>
+            <b className={styles.title}>Phone number</b>
+          </div>
+          <div className={styles.cell}>
+            <b className={styles.title}>Status</b>
+          </div>
+          <div className={styles.cell}>
+            <b className={styles.title}></b>
+          </div>
+        </div>
+        <div className={styles.headerRow}>
+          <div className={styles.cell}>
             <div className={styles.wrap}>
               <img
-                className={styles.img}
+                className={styles.image}
                 src='https://firebasestorage.googleapis.com/v0/b/brand-1b5b2.appspot.com/o/avatar.png?alt=media&token=48dc1426-5e9c-4cfe-9d95-891f2f2b56b2&_gl=1*1mfnhfw*_ga*ODg4OTAzNjM4LjE2OTY5OTQ2OTk.*_ga_CW55HF8NVT*MTY5NzE2NTQ3Ny40LjEuMTY5NzE2NTQ5My40NC4wLjA.'
                 alt='Avatar'
               />
@@ -24,31 +43,29 @@ export const Table: React.FC = (): JSX.Element => {
               </div>
             </div>
           </div>
-          <div className={styles.divCell}>
-            <div className={styles.area}>
-              <b className={styles.title}>Email address</b>
-            </div>
-            <div className={styles.divCell}>email@gmail.com</div>
+          <div className={styles.cell}>
+            <div className={styles.titleCell}>Email address:</div>
+            <div>email@gmail.com</div>
           </div>
-          <div className={styles.divCell}>
-            <div className={styles.area}>
-              <b className={styles.title}>Phone number</b>
-            </div>
-            <div className={styles.divCell}>2012617826</div>
+          <div className={styles.cell}>
+            <div className={styles.titleCell}>Phone number:</div>
+            <div>2012617826</div>
           </div>
-          <div className={styles.divCell}>
-            <div className={styles.area}>
-              <b className={styles.title}>Status</b>
-            </div>
-            <div className={styles.divCell}>
+          <div className={styles.cell}>
+            <div className={styles.titleCell}>Status:</div>
+            <div>
               <Tag color={TAG_COLORS.LIGHT} title='Status' />
             </div>
           </div>
-          <div className={styles.divCell}>
-            <b className={styles.title}></b>
-            <div className={`${styles.divCell} ${styles.button}`}>
-              <Edit />
-              <Delete />
+          <div className={styles.cell}>
+            <div className={styles.titleCell}></div>
+            <div className={styles.button}>
+              <div className={styles.icon}>
+                <Edit />
+              </div>
+              <div className={styles.icon}>
+                <Delete />
+              </div>
             </div>
           </div>
         </div>
