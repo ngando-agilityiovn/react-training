9bfe86a178577ba96db1d1409b5a04e374207467
Ngan.Do <ngan.do@asnet.com.vn>
2023-10-18 11:56:47 +0700

    fix: update code style

--- practices/practice-one/src/pages/Homepage/index.tsx
+++ practices/practice-one/src/pages/Homepage/index.tsx
@@ -81,59 +81,57 @@ const colorStatusMapping = {
   [Status.PENDING]: 'warning'
 };
 
-export const Homepage: React.FC = (): JSX.Element => {
-  return (
-    <main className={homeStyles.homePage}>
-      <Container>
-        <Heading title='Users' className={homeStyles.heading} />
-        <div className={homeStyles.mobileAction}>
-          <p className={homeStyles.copyText}> 14 of 64 results</p>
-          <Button variant={BUTTON_VARIANT.ICON}>
-            <Filter />
-          </Button>
-        </div>
-      </Container>
-      <Container className={homeStyles.tableContainer}>
-        <Table
-          tableHeader={tableHeader}
-          tableData={users}
-          customRender={({ name, avatarUrl, description, email, phoneNumber, status }: User) => {
-            return (
-              <>
-                <div
-                  style={{ flex: '30%' }}
-                  className={combineClasses([homeStyles.tableHeaderContent, homeStyles.userInfo])}
-                >
-                  <UserInformation name={name} image={avatarUrl} description={description} />
-                </div>
-                <div style={{ flex: '30%' }} className={homeStyles.tableHeaderContent}>
-                  <p className='mobile-only text-copy'>Email address</p>
-                  <p>{email}</p>
-                </div>
-                <div style={{ flex: '15%' }} className={homeStyles.tableHeaderContent}>
-                  <p className='mobile-only text-copy'>Phone number</p>
-                  <p>{phoneNumber}</p>
-                </div>
-                <div style={{ flex: '15%' }} className={homeStyles.tableHeaderContent}>
-                  <p className='mobile-only text-copy'>Status</p>
-                  <Tag
-                    title={uppercaseFirstLetter(status)}
-                    variant={colorStatusMapping[status] as ITagProps['variant']}
-                  />
-                </div>
-                <div className={combineClasses([homeStyles.tableHeaderContent, homeStyles.tableActions])}>
-                  <Button variant={BUTTON_VARIANT.ICON}>
-                    <Edit />
-                  </Button>
-                  <Button variant={BUTTON_VARIANT.ICON}>
-                    <Delete />
-                  </Button>
-                </div>
-              </>
-            );
-          }}
-        />
-      </Container>
-    </main>
-  );
-};
+export const Homepage: React.FC = (): JSX.Element => (
+  <main className={homeStyles.homePage}>
+    <Container>
+      <Heading title='Users' className={homeStyles.heading} />
+      <div className={homeStyles.mobileAction}>
+        <p className={homeStyles.copyText}> 14 of 64 results</p>
+        <Button variant={BUTTON_VARIANT.ICON}>
+          <Filter />
+        </Button>
+      </div>
+    </Container>
+    <Container className={homeStyles.tableContainer}>
+      <Table
+        tableHeader={tableHeader}
+        tableData={users}
+        customRender={({ name, avatarUrl, description, email, phoneNumber, status }: User) => {
+          return (
+            <>
+              <div
+                style={{ flex: '30%' }}
+                className={combineClasses([homeStyles.tableHeaderContent, homeStyles.userInfo])}
+              >
+                <UserInformation name={name} image={avatarUrl} description={description} />
+              </div>
+              <div style={{ flex: '30%' }} className={homeStyles.tableHeaderContent}>
+                <p className='mobile-only text-copy'>Email address</p>
+                <p>{email}</p>
+              </div>
+              <div style={{ flex: '15%' }} className={homeStyles.tableHeaderContent}>
+                <p className='mobile-only text-copy'>Phone number</p>
+                <p>{phoneNumber}</p>
+              </div>
+              <div style={{ flex: '15%' }} className={homeStyles.tableHeaderContent}>
+                <p className='mobile-only text-copy'>Status</p>
+                <Tag
+                  title={uppercaseFirstLetter(status)}
+                  variant={colorStatusMapping[status] as ITagProps['variant']}
+                />
+              </div>
+              <div className={combineClasses([homeStyles.tableHeaderContent, homeStyles.tableActions])}>
+                <Button variant={BUTTON_VARIANT.ICON}>
+                  <Edit />
+                </Button>
+                <Button variant={BUTTON_VARIANT.ICON}>
+                  <Delete />
+                </Button>
+              </div>
+            </>
+          );
+        }}
+      />
+    </Container>
+  </main>
+);
