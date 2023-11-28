9e8f58b5ce263c2725b1d199310a336911eb8b69
Ngan.Do <ngan.do@asnet.com.vn>
2023-11-28 09:13:32 +0700

    fix: update typo

--- practices/practice-two/src/layouts/index.ts
+++ practices/practice-two/src/layouts/index.ts
@@ -1,2 +1,1 @@
-export * from './Snavbar'
-export * from '../pages/Home'
+export * from './Sidebar'
--- practices/practice-two/src/pages/Home/index.tsx
+++ practices/practice-two/src/pages/Home/index.tsx
@@ -1,6 +1,12 @@
-import { Box, Button, Container, Flex, Img, Text } from '@chakra-ui/react'
-
-// Icon
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Flex,
+  Img,
+  Text,
+} from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
 
 // Components
@@ -12,11 +18,13 @@ import {
   MenuSelect,
   Notification,
   Search,
-  Snavbar,
   StatusGroup,
 } from '@/components'
 
-const itemList = {
+// Layouts
+import { Sidebar } from '@/layouts'
+
+const menuOptions = {
   listDropdown: [
     {
       id: '0',
@@ -39,10 +47,13 @@ const itemList = {
   ],
 }
 
+const avatar =
+  'https://firebasestorage.googleapis.com/v0/b/icon-foot.appspot.com/o/avatar.png?alt=media&token=81b6abe0-23b2-4e79-b3e1-77050c86baaf'
+
 export const MainLayout = () => {
   return (
     <Container>
-      <Snavbar />
+      <Sidebar />
       <Box pos="absolute" top="0" left="72px" right="0">
         <Flex
           bg="backgroundHeader"
@@ -59,14 +70,12 @@ export const MainLayout = () => {
                 <Active />
               </Box>
             </Box>
-            <Box mx="24px">
+            <Box mx="6">
               <Help />
             </Box>
-            <Img
-              w="24px"
-              h="24px"
-              src="https://firebasestorage.googleapis.com/v0/b/icon-foot.appspot.com/o/avatar.png?alt=media&token=81b6abe0-23b2-4e79-b3e1-77050c86baaf"
-              alt="Avatar active"
+            <Avatar
+              boxSize="6"
+              icon={<Img src={avatar} alt="Avatar active" />}
             />
           </Flex>
         </Flex>
@@ -76,7 +85,7 @@ export const MainLayout = () => {
               leftIcon={<Filter />}
               rightIcon={<Dropdown />}
               title="All"
-              itemList={itemList.listDropdown}
+              itemList={menuOptions.listDropdown}
             />
             <Search />
           </Flex>
