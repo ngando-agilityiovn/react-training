bf511a4708a34ae4460c51ee4d1c385481e066d4
Ngan.Do <ngan.do@asnet.com.vn>
2023-11-29 21:26:55 +0700

    feat: handle show and hide add new project form modal

--- practices/practice-two/src/pages/Projects/index.tsx
+++ practices/practice-two/src/pages/Projects/index.tsx
@@ -1,14 +1,21 @@
 import { AddIcon } from '@chakra-ui/icons'
-import { Box, Button, Flex } from '@chakra-ui/react'
+import { Box, Button, Flex, useDisclosure } from '@chakra-ui/react'
 
 // Components
-import { MenuSelect, Search, StatusGroup } from '@/components'
+import {
+  Form,
+  MenuSelect,
+  ModalCustom,
+  Search,
+  StatusGroup,
+} from '@/components'
 import { DropdownIcon, FilterIcon } from '@/components/Icons'
 
 // Constants
 import { sortOptions } from '@/constants'
 
 const ProjectsPages = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Box>
       <Flex mt="20px" mb="28px" mx="20px" justifyContent="space-between">
@@ -21,11 +28,14 @@ const ProjectsPages = () => {
           />
           <Search width="17.5rem" />
         </Flex>
-        <Button leftIcon={<AddIcon />} variant="solid">
+        <Button leftIcon={<AddIcon />} variant="solid" onClick={onOpen}>
           Add project
         </Button>
       </Flex>
       <StatusGroup />
+      <ModalCustom title="Add project" onClose={onClose} isOpen={isOpen}>
+        <Form />
+      </ModalCustom>
     </Box>
   )
 }
