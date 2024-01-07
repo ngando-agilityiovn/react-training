a0e661526b68b3e0934a62054ba55e31dc345c46
Ngan.Do <ngan.do@asnet.com.vn>
2024-01-07 19:06:27 +0700

    feature: define navbar component

---
+++ practices/practice-three/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { Flex, ListItem, UnorderedList } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
+// Constant
+import { NAVBAR } from '@/constants'
+
+console.log(NAVBAR, 'edff')
+
+const Navbar = () => {
+  return (
+    <UnorderedList listStyleType="none">
+      <Flex>
+        {NAVBAR.map(({ title, link }) => (
+          <ListItem key={title} mr="50px" color="secondary" fontWeight="medium">
+            <Link to={link}>{title}</Link>
+          </ListItem>
+        ))}
+      </Flex>
+    </UnorderedList>
+  )
+}
+
+export default Navbar
