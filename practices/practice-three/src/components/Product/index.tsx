a24fccaa2dc1c0795aff635643903a4c45d9bada
Ngan.Do <ngan.do@asnet.com.vn>
2024-01-09 22:03:51 +0700

    fix: change name constants

--- practices/practice-three/src/components/Product/index.tsx
+++ practices/practice-three/src/components/Product/index.tsx
@@ -1,7 +1,7 @@
 import { Container, Flex } from '@chakra-ui/react'
 
 // Constants
-import { CARDDATA, TEXTDATA } from '@/constants'
+import { CARD_DATA, TEXT_DATA } from '@/constants'
 
 // Components
 import Sidebar from './Sidebar'
@@ -12,7 +12,7 @@ const Product = () => {
     <Container maxW="1280px" pt="49px" px={0}>
       <Flex gap={21}>
         <Sidebar />
-        <ListProduct cardData={CARDDATA} textData={TEXTDATA} />
+        <ListProduct cardData={CARD_DATA} textData={TEXT_DATA} />
       </Flex>
     </Container>
   )
