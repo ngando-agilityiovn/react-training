ec1609be62fb977c61b25e0173adb25561e63b7e
Ngan.Do <ngan.do@asnet.com.vn>
2024-01-11 14:18:33 +0700

    fix: prettier config

--- practices/practice-three/src/components/ProductList/index.tsx
+++ practices/practice-three/src/components/ProductList/index.tsx
@@ -7,7 +7,7 @@ import {
   IconButton,
   Image,
   Stack,
-  Text,
+  Text
 } from '@chakra-ui/react'
 
 //  Icon components
@@ -37,66 +37,68 @@ const ProductList = ({ data }: IProductList) => {
           title,
           price,
           numeric,
-          reviewQuantity: numericRating,
-        }) => (
-          <Card maxW="sm" width="305px" overflow="hidden">
-            <CardBody p="0">
-              <Stack>
-                <Image src={image} alt={alt} width="100%" height="303px" />
-                <IconButton
-                  rounded=""
-                  isRound={true}
-                  variant="solid"
-                  aria-label="Done"
-                  fontSize="20px"
-                  width="46px"
-                  height="46px"
-                  top="12px"
-                  right="8px"
-                  position="absolute"
-                  background="white"
-                  border="none"
-                  _hover={{
-                    background: 'backgroundSuccess ',
-                  }}
-                  icon={<Heart />}
-                />
-              </Stack>
-              <Stack mt="22px" pl="21px" pr="17px">
-                <Flex justify="space-between">
-                  <Text variant="primary">{title}</Text>
-                  <Text fontSize="lg" fontWeight="bold" color="textInactive">
-                    ₹ {price}
+          reviewQuantity: numericRating
+        }) => {
+          return (
+            <Card maxW="sm" width="305px" overflow="hidden">
+              <CardBody p="0">
+                <Stack>
+                  <Image src={image} alt={alt} width="100%" height="303px" />
+                  <IconButton
+                    rounded=""
+                    isRound={true}
+                    variant="solid"
+                    aria-label="Done"
+                    fontSize="20px"
+                    width="46px"
+                    height="46px"
+                    top="12px"
+                    right="8px"
+                    position="absolute"
+                    background="white"
+                    border="none"
+                    _hover={{
+                      background: 'backgroundSuccess '
+                    }}
+                    icon={<Heart />}
+                  />
+                </Stack>
+                <Stack mt="22px" pl="21px" pr="17px">
+                  <Flex justify="space-between">
+                    <Text variant="primary">{title}</Text>
+                    <Text fontSize="lg" fontWeight="bold" color="textInactive">
+                      ₹ {price}
+                    </Text>
+                  </Flex>
+                  <Text color="tertiary" fontWeight="normal" lineHeight="20px">
+                    {numeric}
                   </Text>
-                </Flex>
-                <Text color="tertiary" fontWeight="normal" lineHeight="20px">
-                  {numeric}
-                </Text>
-                <Flex gap="6px">
-                  <Center>
-                    {/* TODO: Implement handle after */}
-                    <Star />
-                    <Star />
-                    <Star />
-                    <Star />
-                    <InActiveStar />
-                  </Center>
-                  <Text color="tertiary" fontSize="sm">
-                    ({numericRating})
-                  </Text>
-                </Flex>
-                <Flex justify="space-between" py="25px" gap="11px">
-                  <Button variant="solid" width="50%">
-                    Add To Cart
-                  </Button>
-                  <Button variant="outline" width="50%">
-                    Add Shorlist
-                  </Button>
-                </Flex>
-              </Stack>
-            </CardBody>
-          </Card>
-        ),
+                  <Flex gap="6px">
+                    <Center>
+                      {/* TODO: Implement handle after */}
+                      <Star />
+                      <Star />
+                      <Star />
+                      <Star />
+                      <InActiveStar />
+                    </Center>
+                    <Text color="tertiary" fontSize="sm">
+                      ({numericRating})
+                    </Text>
+                  </Flex>
+                  <Flex justify="space-between" py="25px" gap="11px">
+                    <Button variant="solid" width="50%">
+                      Add To Cart
+                    </Button>
+                    <Button variant="outline" width="50%">
+                      Add Shorlist
+                    </Button>
+                  </Flex>
+                </Stack>
+              </CardBody>
+            </Card>
+          )
+        }
       )}
     </Flex>
   )
