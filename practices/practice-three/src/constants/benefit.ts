711e9156b610c65062158b85dfb81da9ed876be2
Ngan.Do <ngan.do@asnet.com.vn>
2024-01-15 15:06:01 +0700

    feature: writing unit test and storybook

--- practices/practice-three/src/constants/benefit.ts
+++ practices/practice-three/src/constants/benefit.ts
@@ -1,27 +1,9 @@
 export const BENEFIT_DATA = [
-  {
-    title:
-      'Durable leather is easily cleanable so you can keep your look fresh.'
-  },
-  {
-    title: 'TDurable leather is easily cleanable so you can keep your look f.'
-  },
-  {
-    title:
-      'Toe pieDurable leather is easily cleanable so you can keep your look f durability.'
-  },
-  {
-    title:
-      'Toe Durable leather is easily cleanable so you can keep your look f tern adds durability.'
-  },
-  {
-    title:
-      'Toe piece Durable leather is easily cleanable so you can keep your look f rn adds durability.'
-  },
-  {
-    title: 'Toe piece with star pattern adds durability.'
-  },
-  {
-    title: 'Toe piece with star pattern adds durability.'
-  }
+  'Durable leather is easily cleanable so you can keep your look fresh.',
+  'TDurable leather is easily cleanable so you can keep your look f.',
+  'Toe pieDurable leather is easily cleanable so you can keep your look f durability.',
+  'Toe Durable leather is easily cleanable so you can keep your look f tern adds durability.',
+  'Toe piece Durable leather is easily cleanable so you can keep your look f rn adds durability.',
+  'Toe piece with star pattern adds durability.',
+  'Toe piece with star pattern adds durability.'
 ]
